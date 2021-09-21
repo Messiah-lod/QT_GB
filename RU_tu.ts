3b9685e0367ae7db0d1fc20fa23edccf47d7f2c2
Maks <diablo-lod@mail.ru>
2021-09-21 21:02:28 +0300

    UpdateTranslations

--- RU_tu.ts
+++ RU_tu.ts
@@ -4,69 +4,89 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../../Project/qt_gb/qt_gb/mainwindow.ui" line="14"/>
         <source>MainWindow</source>
-        <translation>Главное окно</translation>
+        <translation type="vanished">Главное окно</translation>
     </message>
 </context>
 <context>
     <name>Settings</name>
     <message>
-        <location filename="settings.cpp" line="90"/>
+        <location filename="settings.cpp" line="279"/>
         <source>Dark theme</source>
         <translation>Темная тема</translation>
     </message>
     <message>
-        <location filename="settings.cpp" line="91"/>
+        <location filename="settings.cpp" line="280"/>
         <source>Set Russian Language</source>
         <translation>Установить английский язык</translation>
     </message>
     <message>
-        <location filename="settings.cpp" line="92"/>
+        <location filename="settings.cpp" line="281"/>
         <source>Settings</source>
         <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="settings.cpp" line="283"/>
+        <source>Hot Key &quot;Open&quot;</source>
+        <translation>Горячая клавиша &quot;Открыть&quot;</translation>
+    </message>
+    <message>
+        <location filename="settings.cpp" line="284"/>
+        <source>Hot Key &quot;Save&quot;</source>
+        <translation>Горячая клавиша &quot;Сохранить&quot;</translation>
+    </message>
+    <message>
+        <location filename="settings.cpp" line="285"/>
+        <source>Hot Key &quot;Clear Text Editor&quot;</source>
+        <translation>Горячая клавиша &quot;Очистить&quot;</translation>
+    </message>
+    <message>
+        <location filename="settings.cpp" line="286"/>
+        <source>Hot Key &quot;Exit&quot;</source>
+        <translation>Горячая клавиша &quot;Выход&quot;</translation>
     </message>
 </context>
 <context>
     <name>TextEditor</name>
     <message>
-        <location filename="texteditor.cpp" line="121"/>
-        <location filename="texteditor.cpp" line="123"/>
+        <location filename="texteditor.cpp" line="155"/>
+        <location filename="texteditor.cpp" line="157"/>
         <source>Open</source>
         <translation>Открыть</translation>
     </message>
     <message>
-        <location filename="texteditor.cpp" line="122"/>
+        <location filename="texteditor.cpp" line="156"/>
         <source>Open Read only</source>
         <translation>Открыть для чтения</translation>
     </message>
     <message>
-        <location filename="texteditor.cpp" line="124"/>
+        <location filename="texteditor.cpp" line="158"/>
         <source>Save</source>
         <translation>Сохранить</translation>
     </message>
     <message>
-        <location filename="texteditor.cpp" line="125"/>
+        <location filename="texteditor.cpp" line="138"/>
+        <location filename="texteditor.cpp" line="159"/>
         <source>Help</source>
         <translation>Справка</translation>
     </message>
     <message>
-        <location filename="texteditor.cpp" line="126"/>
+        <location filename="texteditor.cpp" line="160"/>
         <source>Settings</source>
         <translation>Настройки</translation>
     </message>
     <message>
-        <location filename="texteditor.cpp" line="120"/>
+        <location filename="texteditor.cpp" line="154"/>
         <source>Text editor</source>
         <translation>Текстовый редактор</translation>
     </message>
     <message>
-        <location filename="texteditor.cpp" line="58"/>
+        <location filename="texteditor.cpp" line="95"/>
         <source>Open file</source>
         <translation>Открыть файл</translation>
     </message>
     <message>
-        <location filename="texteditor.cpp" line="80"/>
+        <location filename="texteditor.cpp" line="117"/>
         <source>Save file</source>
         <translation>Сохранить файл</translation>
     </message>
